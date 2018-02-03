c56f39cc5c855b8e9f0f77b28a0d8208144cca30
ZHANG Yu <octopusthu@gmail.com>
2018-02-03 20:28:22 +0800

    Upgrading to spring-boot-2.0.

---
+++ ejw-demo/src/main/java/com/octopusthu/ejw/demo/web/back/DemoBackController.java
@@ -0,0 +1,14 @@
+package com.octopusthu.ejw.demo.web.back;
+
+import com.octopusthu.ejw.web.back.controller.BackBaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoBackController extends BackBaseController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+}
