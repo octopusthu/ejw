72c024a6da644b481b52d68e484fe3fc69847734
ZHANG Yu <octopusthu@gmail.com>
2019-08-12 16:53:07 +0800

    Added two quizes.

---
+++ ejw-sample/src/main/java/com/octopusthu/ejw/sample/quiz/RemoveDuplicateElements.java
@@ -0,0 +1,22 @@
+package com.octopusthu.ejw.sample.quiz;
+
+
+import java.util.Set;
+
+public interface RemoveDuplicateElements {
+
+    /**
+     * 找出在两个集合a、b中只出现过一次的元素，以新集合的形式返回。
+     *
+     * @return 新的集合。不能为 <code>null</code> 。
+     */
+    Set<Integer> removeDuplicateElements(Set<Integer> a, Set<Integer> b);
+}
+
+class RemoveDuplicateElementsImpl implements RemoveDuplicateElements {
+
+    @Override
+    public Set<Integer> removeDuplicateElements(Set<Integer> a, Set<Integer> b) {
+        return null;
+    }
+}
